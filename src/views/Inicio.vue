eadb4b62ea7809b3a042042b83f9669b238daade
Isadora Dornelas Teixeira Silva <isadora1405@gmail.com>
2021-10-02 13:45:34 -0300

    Inicio modificações

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,16 +1,25 @@
 <template>
     <v-container>
-        <h2 class="text-h5 text-center mb-3 mt-5">Você não está sozinho</h2>
+        <h2 class="text-h5 text-center mb-3 mt-5">Uma conversa, um abraço ou um simples "estou aqui", pode salvar muitas vidas!</h2>
         <v-img class="inicio-imagem mb-4"
         src="https://lh3.googleusercontent.com/proxy/H1nWTjnOwAuclzVZlWlpMO8KhI15V0HWJTf6O5QnDw3Byakb6k9zGPTm2O6f4GjaRhGl2NQmoBa6mJ90u-IBDON0ZKq_hwx2G16DeYRWUEdsCxTyU5mbbBgbUHAb">
         </v-img>
-        <p></p>
+        <div> 
+            <v-icon x-large>mdi-comment-multiple</v-icon>
+            <p>Busque informações, procure ajuda, fale abertamente sobre as emoções.</p>
+        </div>
+        
     </v-container>
 </template>
 
 <script>
 export default {
-    name: 'Inicio'
+    name: 'Inicio',
+    data() {
+    return {
+      group: null,
+    }
+  }
 }
 </script>
 
