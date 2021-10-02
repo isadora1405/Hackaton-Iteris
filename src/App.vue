77853f3fb9de7489c9d0eb874b4caa64b07a02b8
Rafael Ribeiro Junqueira <rafaelrjunqueira@gmail.com>
2021-10-02 12:53:08 -0300

    TopBar

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,47 @@
     <v-app-bar
       color="yellow darken-2 accent-4"
       dense
-      dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
+      dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <v-toolbar-title>Setembro Amarelo</v-toolbar-title>
-
     </v-app-bar>
+<v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="yellow--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-hand-heart-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Quero ajuda!</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-allergy</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Sintomas</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view/>
     </v-main>
@@ -21,7 +55,8 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
