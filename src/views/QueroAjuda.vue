a02a005fbb63985648ebdffe6fa063037fed68f1
Rafael Ribeiro Junqueira <rafaelrjunqueira@gmail.com>
2021-10-02 16:38:37 -0300

    texto alinhado a esquerda

--- src/views/QueroAjuda.vue
+++ src/views/QueroAjuda.vue
@@ -4,34 +4,34 @@
         <v-img class="imagem ajuda mb-4"
             src="https://static.tuasaude.com/media/article/db/cb/como-identificar-um-comportamento-suicida_23270_l.jpg">
         </v-img>
-        <h4>De acordo com a Organização Mundial de Saúde (OMS), quase 800 mil pessoas morrem por suicídio todos os anos.</h4>
-           <h4 class="mb-15">Reconheça os sintomas que podem causar suicídio, busque ajuda e salve vidas! 
+        <p>De acordo com a Organização Mundial de Saúde (OMS), quase 800 mil pessoas morrem por suicídio todos os anos.</p>
+           <p class="mb-15">Reconheça os sintomas que podem causar suicídio, busque ajuda e salve vidas! 
                <v-icon>mdi-hand-heart-outline</v-icon>
-           </h4>
+           </p>
         
-        <h4>1. Tristeza excessiva e falta de vontade para estar com outras pessoas</h4>
-        <p>Estar frequentemente triste e sem vontade para participar em atividades com os amigos ou fazer o que se fazia
+        <h4 class="text-left">1. Tristeza excessiva e falta de vontade para estar com outras pessoas</h4>
+        <p class="text-left">Estar frequentemente triste e sem vontade para participar em atividades com os amigos ou fazer o que se fazia
              antigamente são alguns sintomas de depressão, que, quando não tratada, é uma das principais causas do suicídio.</p>
         
-        <h4>2. Alteração repentina do comportamento com uso de roupa muito diferente do habitual, por exemplo</h4>
-        <p>Uma pessoa com ideias suicidas pode comportar-se de forma diferente do habitual, falando de outra forma, 
+        <h4 class="text-left">2. Alteração repentina do comportamento com uso de roupa muito diferente do habitual, por exemplo</h4>
+        <p class="text-left">Uma pessoa com ideias suicidas pode comportar-se de forma diferente do habitual, falando de outra forma, 
             deixando de conseguir entender o humor de uma conversa ou, até, participando em atividades de risco, 
             como utilizar drogas, ter contato íntimo desprotegido ou dirigir a grande velocidade.</p>
 
-        <h4>3. Tratar de vários assuntos pendentes ou fazer um testamento</h4>
-        <p>Quando alguém está pensando cometer suicídio é comum começar a fazer várias tarefas para tentar organizar
+        <h4 class="text-left">3. Tratar de vários assuntos pendentes ou fazer um testamento</h4>
+        <p class="text-left">Quando alguém está pensando cometer suicídio é comum começar a fazer várias tarefas para tentar organizar
              sua vida e terminar assuntos pendentes, como faria se fosse viajar por muito tempo ou viver para outro país. 
              Alguns exemplos são visitar familiares que já não vê há muito tempo, pagar pequenas dívidas ou oferecer vários
               objetos pessoais, por exemplo.</p>
 
-        <h4>4. Demonstrar calma ou despreocupação depois de um período de grande tristeza ou depressão</h4>
-        <p>Algumas mudanças no comportamento como alterações do sono, apetite e demonstrar um comportamento calmo
+        <h4 class="text-left">4. Demonstrar calma ou despreocupação depois de um período de grande tristeza ou depressão</h4>
+        <p class="text-left">Algumas mudanças no comportamento como alterações do sono, apetite e demonstrar um comportamento calmo
              e despreocupado depois de um período de grande tristeza, depressão ou ansiedade, pode ser um sinal de que
               a pessoa está pensando no suicídio. Isso acontece porque a pessoa pensa ter encontrado a solução para o seu problema,
                deixando de se sentir tão preocupada.</p>
 
-        <h4>5. Fazer ameaças de suicídio frequentes</h4>
-        <p>A maior parte das pessoas que estão pensando em suicídio irão informar um amigo ou familiar das suas intenções. 
+        <h4 class="text-left">5. Fazer ameaças de suicídio frequentes</h4>
+        <p class="text-left">A maior parte das pessoas que estão pensando em suicídio irão informar um amigo ou familiar das suas intenções. 
             Embora esse comportamento, muitas vezes, seja visto como uma forma de chamar a atenção, nunca deve ser ignorado, 
             especialmente se a pessoa está vivendo uma fase de depressão ou de grandes alterações na sua vida.</p>
 
@@ -46,8 +46,7 @@ export default {
 </script>
 
 <style>
-
-h4{
+    h4{
         font-family: 'tahoma';
     };
         p{
